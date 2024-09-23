dd4d929b5f641ae2908e654e1dfaef9ebe35eb04
kyy <kyyyy8629@gmail.com>
2024-09-23 17:29:23 +0900

    update

--- getting-started-todo-app/backend/src/routes/team.js
+++ getting-started-todo-app/backend/src/routes/team.js
@@ -1,4 +1,4 @@
 module.exports = async (req, res) => {
     // const items = await db.getItems();
-    res.send("hello world, 채승, 기훈, 유열 ,채빈, 동관");
+    res.send("hello world, 채승, 기훈, 유열 ,채빈, 동관 파이팅");
 };
